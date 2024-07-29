903619541e809c1348acb71fd3b93ceb4e6c2ae4
Paulo Ferrari <pf.devcon@gmail.com>
2024-07-29 19:47:04 -0300

    increase duration to 60 secs in orange_predict.py

--- orange_predict.py
+++ orange_predict.py
@@ -3,13 +3,13 @@ import pickle
 import numpy as np
 import soundfile as sf
 from filter import record_audio, play_audio, audio_normalized
-from features import get_audio_features, cleanAudioData, create_directory, create_data_table_header_clean, create_instance_clean
+from features import cleanAudioData, create_data_table_header_clean, create_instance_clean
 from create_csv import create_csv_file
 
 excerpt_duration = 5 #! NAO ALTERAR ESSE PARAMETRO!!!
 sr = 48000 #! NAO ALTERAR ESSE PARAMETRO!!!
 offset = sr * excerpt_duration
-duration = 30 #? alterar de acordo com o tempo total de amostragem
+duration = 60 #? alterar de acordo com o tempo total de amostragem
 intervals = int(duration / excerpt_duration)
 data_path = './datasets/DATA.csv' #? alterar de acordo com seu diretorio
 csvfile, writer = create_csv_file(data_path)
@@ -41,7 +41,7 @@ for i in range(intervals):
 csvfile.close()
 
 # Load the saved model
-model_path = "./orange_NN_model.pkcls"
+model_path = "./orange_NN_model_4singers.pkcls"
 with open(model_path, "rb") as file:
     model = pickle.load(file)
 
@@ -63,18 +63,3 @@ for prediction in predictions:
     p = int(prediction)
     print('prediction:', p, '- classe:', classes[p])
 
-# # # Example NumPy array (replace with your actual data)
-# # Assume this is a 2D array where rows are samples and columns are features
-# numpy_data = np.array([audio_features])
-# print('numpy_data:', numpy_data, 'len:', len(numpy_data))
-
-# # # Convert NumPy array to Orange data Table
-# domain = model.domain
-# orange_data = Orange.data.Table(domain, numpy_data)
-
-# # Make predictions
-# predictions = model(orange_data)
-
-# # Print predictions
-# for prediction in predictions:
-#     print(prediction)
